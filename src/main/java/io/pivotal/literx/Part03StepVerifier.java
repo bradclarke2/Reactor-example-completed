ce4c44f662530d950c42414e5239ede767de3cb3
bradleyclarke <gce5>
2017-08-23 16:31:46 +0530

    test 3 changed, 4 and 5 completed

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -16,11 +16,13 @@
 
 package io.pivotal.literx;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 import io.pivotal.literx.domain.User;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import reactor.test.StepVerifier.Assertions;
 
 /**
  * Learn how to use StepVerifier to test Mono, Flux or any other kind of Reactive Streams Publisher.
@@ -35,14 +37,18 @@ public class Part03StepVerifier {
 
   // TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
   void expectFooBarComplete(Flux<String> flux) {
-    StepVerifier.create(Flux.just("foo", "bar"));
+    StepVerifier.create(flux)
+        .expectNext("foo", "bar")
+        .verifyComplete();
   }
 
 //========================================================================================
 
   // TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
   void expectFooBarError(Flux<String> flux) {
-    StepVerifier.create(Flux.just("foo", "bar", Flux.error(new RuntimeException())));
+    StepVerifier.create(flux)
+        .expectNext("foo", "bar")
+        .verifyError(RuntimeException.class);
   }
 
 //========================================================================================
@@ -50,14 +56,19 @@ public class Part03StepVerifier {
   // TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
   // and another one with "jpinkman" then completes successfully.
   void expectSkylerJesseComplete(Flux<User> flux) {
-    StepVerifier.create(flux);
+    StepVerifier.create(flux)
+        .expectNextMatches(user -> user.getUsername().equals("swhite"))
+        .expectNextMatches(user -> user.getUsername().equals("ipinkman"))
+        .expectComplete();
   }
 
 //========================================================================================
 
   // TODO Expect 10 elements then complete and notice how long the test takes.
   void expect10Elements(Flux<Long> flux) {
-    Flux.just(flux);
+    StepVerifier.create(flux)
+        .expectNextCount(10)
+        .verifyComplete();
   }
 
 //========================================================================================
@@ -65,7 +76,10 @@ public class Part03StepVerifier {
   // TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
   // by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
   void expect3600Elements(Supplier<Flux<Long>> supplier) {
-    Flux.just(supplier);
+    StepVerifier.withVirtualTime(supplier)
+        .thenAwait(Duration.ofSeconds(1))
+        .expectNextCount(3600)
+        .verifyComplete();
   }
 
   private void fail() {
--- src/main/java/io/pivotal/literx/Part05Merge.java
+++ src/main/java/io/pivotal/literx/Part05Merge.java
@@ -13,23 +13,23 @@ public class Part05Merge {
 
 //========================================================================================
 
-	// TODO Merge flux1 and flux2 values with interleave
-	Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
-	}
+  // TODO Merge flux1 and flux2 values with interleave
+  Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
+    return Flux.merge(flux1, flux2);
+  }
 
 //========================================================================================
 
-	// TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
-	Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
-		return null;
-	}
+  // TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
+  Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
+    return Flux.mergeSequential(flux1, flux2);
+  }
 
 //========================================================================================
 
-	// TODO Create a Flux containing the value of mono1 then the value of mono2
-	Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
-	}
+  // TODO Create a Flux containing the value of mono1 then the value of mono2
+  Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
+    return Flux.concat(mono1, mono2);
+  }
 
 }
